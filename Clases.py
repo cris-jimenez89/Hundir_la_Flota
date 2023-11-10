89727a8f76b83610945546e3a3199ee5d4096c7a
cris-jimenez89 <cjimpar@gmail.com>
2023-11-10 15:42:42 +0100

    Creamos esquema de clases con los metodos que usaremos

---
+++ Clases.py
@@ -0,0 +1,38 @@
+import numpy as np
+import random
+import winsound
+from icecream import ic
+
+class Tablero:
+    
+    def __init__(self) -> None:
+        self.numero_impactos 
+        self.tablero_barcos 
+        self.tablero_jugadas 
+        self.posiciones 
+        self.tipos_barcos 
+        self.posiciones_probadas 
+        
+    
+    def colocar_barcos(self):
+       
+    
+    def pintar_tablero_barcos(self):
+    
+
+    def pintar_tablero_jugadas(self):
+        
+
+    def pintar_barco(self,posiciones):
+        
+    def pintar_resultado_disparo(self,resultado):
+        
+        
+    
+    def recibir_impacto(self,pos):
+        
+    def estado_jugador(self):
+        
+    def disparar(self):
+        
+    
