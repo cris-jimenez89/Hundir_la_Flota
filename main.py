84697b711970f4ff5599ec7bbfe213b58cbddf5b
ricardogonzalezotal <ricardogotal@gmail.com>
2023-11-14 17:27:42 +0100

    añado metodo main a main.py

--- main.py
+++ main.py
@@ -78,3 +78,13 @@ class Juego:
           except Exception:
              print("introduzca (s/n)")
     
+    def main():
+        condicion = True
+        while (condicion):
+            mi_juego = Juego()
+            mi_juego.iniciar_juego()
+            mi_juego.jugar()
+            condicion = mi_juego.jugar_otra_vez()
+
+    if __name__ == "__main__":
+        main()
