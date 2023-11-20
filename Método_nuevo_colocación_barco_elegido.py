3392c1a8464deefa1af0ff046742dc8e7ed88b56
Rocio CR <145597024+Rociocr94@users.noreply.github.com>
2023-11-20 16:15:06 +0100

    Update Método_nuevo_colocación_barco_elegido.py

--- Método_nuevo_colocación_barco_elegido.py
+++ Método_nuevo_colocación_barco_elegido.py
@@ -5,7 +5,7 @@ class Tablero:
         self.listaposiciones = []  # Lista de tuplas, todas las B que hay en el tablero
         self.tipos_barcos = {1: 4, 2: 3, 3: 2, 4: 1}
 
-def colocar_barco_elegido(self):
+def colocar_barco_aleatorio_o_elegido(self):
     '''Método para colocar los barcos en el tablero barcos tras elegir posicion
     Input: No tiene
     Output:No tiene, sirve para pintar los barcos tras  generar sus posiciones
