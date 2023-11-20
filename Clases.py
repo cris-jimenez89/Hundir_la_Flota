5c28849d895854cf2851f8ddc633291ddc89e921
cris-jimenez89 <145456716+cris-jimenez89@users.noreply.github.com>
2023-11-20 12:33:42 +0100

    modificacion de error de comillas
    modificacion de error de comillas, error de string

--- Clases.py
+++ Clases.py
@@ -585,14 +585,14 @@ class Juego:
         self.nivel_dificultad = self.elegir_nivel()
         #ic("Para que jugador 1 elija que tipo de metodo usará para colocar los barcos")
         opcion_colocacion = self.elegir_metodo_colocacion()
-        if opcion_colocacion == "1":
+        if opcion_colocacion == 1:
             self.jugador1.generar_flota_random()# Posiciones aleatorias
             self.jugador1.colocar_barco_aleatorio_o_elegido()
             self.jugador2.generar_flota_random()# Posiciones aleatorias
             self.jugador2.colocar_barco_aleatorio_o_elegido()
-        elif opcion_colocacion == "2":
+        elif opcion_colocacion == 2:
             self.colocar_barcos()# Posiciones fijas            
-        elif opcion_colocacion == "3":
+        elif opcion_colocacion == 3:
             self.jugador1.colocar_barcos_fijos() #NOMBRE DE METODO ROCIO# eligiendo nosotros la posicion, PENDIENTE DE INCLUIR
             self.jugador2.colocar_barcos_fijos() #NOMBRE DE METODO ROCIO# eligiendo nosotros la posicion, PENDIENTE DE INCLUIR
             
